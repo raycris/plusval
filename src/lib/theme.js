c3b99d3797165b47364ab00bf1e45a06d815701b
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-08-23 15:59:34 -0400

    footer completed

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,14 +1,15 @@
 const theme = {
   color: {
     white: "#FFFFFF",
+    orage: "#e16b31",
     primary: "#86c458",
     secundary: "#4c4c4c",
-    laghtGray: "#5d5d5d"
+    laghtGray: "#5d5d5d",
   },
   fontSize: {
+    base: "18px",
     small: "14px",
     normal: "16px",
-    base: "18px",
     subtitle: "20px",
   },
 };
